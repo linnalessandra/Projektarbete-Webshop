fe74a74b4af3eab8fb537625c16ab89d54932e7b
linnalessandra <linnalessandra@outlook.com>
2021-02-23 12:06:50 +0100

    Renderat ut produkterna

--- index.js
+++ index.js
@@ -2,14 +2,70 @@ window.addEventListener("load", initSite)
 
 async function initSite() {
    const products = await makeRequest("./api/recievers/productReciever.php", "GET")
-   console.log(products)
+   showProducts(products)
+   
 }
 
 /* visa produkterna på startsidan */
 async function displayProducts() {
     const products = await makeRequest("./api/recievers/productReciever.php", "GET")
-    console.log(products)
+    
+    //console.log(products)
 }
+
+function showProducts(products){
+    console.log(products)
+    let computerDiv = document.getElementById("datorer")
+    products.forEach(product => {
+    let title = document.createElement("h2")
+    title.innerText = product.productName
+    let imageHolder = document.createElement("div")
+    imageHolder.classList.add("imageHolder")
+    imageHolder.innerHTML = "<img src='" + "./productImages/" + product.imageSrc + "'>"
+    let productDescription = document.createElement("p")
+    productDescription.innerText = product.productDescription
+    let productPrice = document.createElement("h3")
+    productPrice.innerText = product.productPrice + " kr" 
+    let productToCartButton = document.createElement("button")
+    productToCartButton.innerText = "Lägg till i kundvagn"
+    productToCartButton.addEventListener("click",() => {
+        let productToCart = {
+            productID: product.productID, 
+            productName: product.productName,
+            productPrice: product.productPrice,
+            productDescription: product.productDescription,
+            imageSrc: product.imageSrc
+        }
+        console.log(productToCart)
+
+        let currentCart = localStorage.getItem("cart") 
+        currentCart = JSON.parse(currentCart)
+        if(currentCart == null ) {
+            currentCart = []
+        }
+
+        let foundIndex = currentCart.findIndex((cartItem) => {
+            return cartItem.product.productID == productToCart.productID
+        })
+        if(foundIndex != -1) {
+            currentCart[foundIndex].quantity++
+        } else {
+            currentCart.push({
+                product: productToCart, 
+                quantity: 1
+            })
+        }
+       
+        localStorage.setItem("cart", JSON.stringify(currentCart))
+
+    } )
+    computerDiv.append(title, imageHolder, productDescription, productPrice, productToCartButton)
+
+    });
+
+}
+
+
 
 
 /* async function deleteAllProducts() {
